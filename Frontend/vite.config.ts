5842e8706a13b9fc11a951ac719fb8301486fd52
유아름 <foottable@gmail.com>
2025-10-23 17:23:30 +0900

    chore: zustand, tailwind 설치 및 설정

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,10 +11,12 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+      "@shared": path.resolve(__dirname, "src/shared"),
     },
   },
   build: {
