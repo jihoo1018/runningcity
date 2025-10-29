048163dd0a5c9b3e7ff354656859fd4cc15997d7
junseo <dpdl33000@naver.com>
2025-10-29 14:26:50 +0900

    rename:cors  폴더위치 수정

--- Backend/src/main/java/com/runningcity/global/common/CorsConfig.java
+++ Backend/src/main/java/com/runningcity/global/common/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.runningcity.mission.config;
+package com.runningcity.common;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
