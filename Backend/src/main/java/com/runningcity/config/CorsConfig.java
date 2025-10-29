8e2b2bc00e5ab136a1e9750d0cca204efa53a4e9
jihoo <wendy991018@naver.com>
2025-10-29 17:50:46 +0900

    fix : cors 위치 변경

--- Backend/src/main/java/com/runningcity/config/CorsConfig.java
+++ Backend/src/main/java/com/runningcity/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.runningcity.global.common.config;
+package com.runningcity.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
