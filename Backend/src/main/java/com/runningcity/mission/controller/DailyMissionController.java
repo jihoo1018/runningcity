064bb551aa02298b9122849d3f57da502af8e1d5
junseo <dpdl33000@naver.com>
2025-10-30 16:24:17 +0900

    fix:controller mapping 수정

--- Backend/src/main/java/com/runningcity/mission/controller/DailyMissionController.java
+++ Backend/src/main/java/com/runningcity/mission/controller/DailyMissionController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/users/{userId}/daily-missions")
+@RequestMapping("/users/{userId}/daily-missions")
 public class DailyMissionController {
 
     private final DailyMissionService dailyMissionService;
