4057da4729bb1da4f7c38f5ef3c93cd75b5f14f5
유아름 <foottable@gmail.com>
2025-11-07 18:43:58 +0900

    Fix: 빌드 에러 최소 수정

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,6 +8,4 @@ plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.25" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.android.library) apply false
 }
