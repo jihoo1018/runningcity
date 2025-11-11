0f5936abd3754283de31f1f2e6b45de67b04915c
유아름 <foottable@gmail.com>
2025-11-11 10:53:59 +0900

    Fix: 웹뷰 렌더링 에러 수정

--- Frontend/src/app/index.tsx
+++ Frontend/src/app/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "../shared/styles/tailwind.css";
+import "@/shared/styles/tailwind.css";
 
 export function renderApp() {
   createRoot(document.getElementById("root")!).render(
