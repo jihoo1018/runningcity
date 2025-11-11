0e3c56b62a1f1ba8d46a046714c652af9b804654
유아름 <foottable@gmail.com>
2025-11-11 15:53:23 +0900

    Chore: svgr 의존성 설치 및 설정

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [
@@ -10,6 +11,7 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    svgr(),
     tailwindcss(),
   ],
   resolve: {
