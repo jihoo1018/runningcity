43c27e1c7286ae231dd99ac038b9b7fc4d4c9f14
유아름 <foottable@gmail.com>
2025-11-12 14:46:27 +0900

    Feat: 공통 스타일 버튼 컴포넌트 추가

---
+++ Frontend/src/shared/ui/CommonButton.tsx
@@ -0,0 +1,28 @@
+import clsx from "clsx";
+
+type props = {
+  variant: "outline" | "solid";
+  children: React.ReactNode;
+  className?: string;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const CommonButton = ({ children, variant = "outline", className, ...rest }: props) => {
+  return (
+    <button
+      {...rest}
+      className={clsx(
+        "text-button flex w-full justify-center rounded-lg px-5 py-3",
+        variant === "outline" &&
+          "border-primary bg-section-bg active:bg-primary active:text-custom-black border",
+        variant === "solid" && "bg-custom-white text-custom-black active:opacity-80",
+        rest.disabled &&
+          "border-custom-gray/50 text-custom-gray/50 bg-section-bg pointer-events-none cursor-not-allowed border",
+        className,
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default CommonButton;
