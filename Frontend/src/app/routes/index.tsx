f2b809a3103ff3394f5581b548a7c7609b95edbd
정민주 <coabbo@naver.com>
2025-11-13 17:44:52 +0900

    feat: 프론트 달리는 중 화면 구현

--- Frontend/src/app/routes/index.tsx
+++ Frontend/src/app/routes/index.tsx
@@ -12,6 +12,7 @@ import BoutiquePage from "@/pages/boutique";
 import ShowroomPage from "@/pages/showroom";
 import ShowroomMePage from "@/pages/showroom/me";
 import ShowroomEditPage from "@/pages/showroom/edit";
+import RunningPage from "@/pages/running";
 
 import { ModalLayer } from "../modal/ModalLayer";
 
@@ -24,7 +25,7 @@ export function AppRoutes() {
     <>
       <Routes location={background || location}>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<RunningPage />} />
           <Route path="/report" element={<RecordListPage />} />
           <Route path="/report/:sid" element={<ReportDetailPage />} />
           <Route path="/entry" element={<EntryPage />} />
@@ -39,6 +40,7 @@ export function AppRoutes() {
 
         <Route path="/nickname" element={<NicknamePage />} />
         <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/running" element={<RunningPage />} />
       </Routes>
 
       {background && (
