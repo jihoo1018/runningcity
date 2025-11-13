8c54249f8eb6df2039bbc58a6c39789f262ad3ea
유아름 <foottable@gmail.com>
2025-11-13 16:39:11 +0900

    Feat: 공통 모달 데이터 전달 기능 추가

--- Frontend/src/app/modal/registry.tsx
+++ Frontend/src/app/modal/registry.tsx
@@ -1,12 +1,20 @@
 import { lazy } from "react";
 import type { FlowRegistry } from "./types";
 
-const SettingsRoot = lazy(() => import("@/features/modals/setting/Root"));
-const SettingsConfirm = lazy(() => import("@/features/modals/setting/Confirm"));
+const SettingRoot = lazy(() => import("@/features/modals/setting/Root"));
+const SettingConfirm = lazy(() => import("@/features/modals/setting/Confirm"));
+const SettingWithdraw = lazy(() => import("@/features/modals/setting/Withdraw"));
+const SettingGoal = lazy(() => import("@/features/modals/setting/Goal"));
+const AlertRoot = lazy(() => import("@/features/modals/alert/Root"));
 
 export const REGISTRY = {
   setting: {
-    root: SettingsRoot,
-    confirm: SettingsConfirm,
+    root: SettingRoot,
+    confirm: SettingConfirm,
+    withdraw: SettingWithdraw,
+    goal: SettingGoal,
+  },
+  alert: {
+    root: AlertRoot,
   },
 } satisfies FlowRegistry;
