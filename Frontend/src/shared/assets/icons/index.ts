a2f27b35decb7b707af61a097d9c5208df593ad7
유아름 <foottable@gmail.com>
2025-11-14 16:39:59 +0900

    Chore: warning icon 에셋 추가

--- Frontend/src/shared/assets/icons/index.ts
+++ Frontend/src/shared/assets/icons/index.ts
@@ -20,3 +20,4 @@ export { ReactComponent as TimeIcon } from "./time.svg";
 export { ReactComponent as VolumeUpIcon } from "./volume_on.svg";
 export { ReactComponent as VolumeOffIcon } from "./volume_off.svg";
 export { ReactComponent as SpinnerIcon } from "./spinner.svg";
+export { ReactComponent as WarningIcon } from "./warning.svg";
