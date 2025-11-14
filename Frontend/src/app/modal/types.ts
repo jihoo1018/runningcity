848059ff5db705f5efad84943c21f6140ccc2a04
유아름 <foottable@gmail.com>
2025-11-14 16:48:28 +0900

    Fix: 모달 타입 추가

--- Frontend/src/app/modal/types.ts
+++ Frontend/src/app/modal/types.ts
@@ -3,6 +3,7 @@ export const FLOWS = {
   alert: ["root"] as const,
   user: ["modify", "confirm"] as const,
   mission: ["root", "confirm"] as const,
+  entry: ["root", "level"] as const,
 } as const;
 
 export type FlowKey = keyof typeof FLOWS;
