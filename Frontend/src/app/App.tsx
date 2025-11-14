39d3e20ba248635021b4792bc9db34d4a051a821
유아름 <foottable@gmail.com>
2025-11-14 09:47:43 +0900

    Fix: 해시 라우터로 변경

--- Frontend/src/app/App.tsx
+++ Frontend/src/app/App.tsx
@@ -1,16 +1,15 @@
-// src/app/App.tsx
 console.log("✅ React App Loaded");
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Providers } from "./providers";
 import { AppRoutes } from "./routes";
 
 function App() {
   return (
     <Providers>
-      <BrowserRouter>
+      <HashRouter>
         <AppRoutes />
-      </BrowserRouter>
+      </HashRouter>
     </Providers>
   );
 }
