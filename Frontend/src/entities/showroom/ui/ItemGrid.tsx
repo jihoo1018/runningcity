20782319f82fc3723f9eff9670c7e55841c8d523
이유리 <youree1226@naver.com>
2025-11-15 11:15:45 +0900

    feat: 옷갈아입기 프론트 작업중 feat: modal type추가 fix: 쇼룸 페이지 css 수정

--- Frontend/src/entities/showroom/ui/ItemGrid.tsx
+++ Frontend/src/entities/showroom/ui/ItemGrid.tsx
@@ -6,32 +6,37 @@ type ItemGridProps = {
   onNext: () => void;
 };
 
+// src/entities/showroom/ui/ItemGrid.tsx
+
 export const ItemGrid = ({ items, onPrev, onNext }: ItemGridProps) => {
   return (
-    <div className="relative">
+    <div className="relative mt-2">
       {/* prev 버튼 */}
       <button
-        className="absolute top-1/2 left-0 h-12 w-12 -translate-y-1/2 rounded-full border bg-white"
+        className="bg-primary absolute top-1/2 left-0 h-7 w-7 -translate-y-1/2 rounded-full shadow sm:h-12 sm:w-12"
         onClick={onPrev}
       >
-        prev
+        ◀
       </button>
 
       {/* grid */}
-      <div className="border-primary mt-1 grid grid-cols-3 gap-4 p-4">
+      <div className="border-primary mt-1 grid grid-cols-4 gap-3 p-4 sm:grid-cols-4 sm:gap-4 md:grid-cols-5">
         {items.map((item, idx) => (
-          <div key={idx} className="flex h-20 w-20 items-center justify-center border">
-            {item || ""}
+          <div
+            key={idx}
+            className="flex h-16 w-16 items-center justify-center rounded-lg border sm:h-20 sm:w-20 md:h-24 md:w-24"
+          >
+            {item}
           </div>
         ))}
       </div>
 
       {/* next 버튼 */}
       <button
-        className="absolute top-1/2 right-0 h-12 w-12 -translate-y-1/2 rounded-full border bg-white"
+        className="bg-primary absolute top-1/2 right-0 h-7 w-7 -translate-y-1/2 rounded-full border sm:h-12 sm:w-12"
         onClick={onNext}
       >
-        next
+        ▶
       </button>
     </div>
   );
