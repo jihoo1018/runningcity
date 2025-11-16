0b83cf743f9b0d298c24114481b6e0052b9fc846
유아름 <foottable@gmail.com>
2025-11-16 14:58:20 +0900

    Chore: Crashlytics 라이브러리 의존성 추가

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.googleServices) apply false
+    alias(libs.plugins.firebaseCrashlyticsPlugin) apply false
 }
