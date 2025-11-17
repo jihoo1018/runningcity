77e920fc471415387f00760779416369b7c0a2f4
이유리 <youree1226@naver.com>
2025-11-17 14:39:23 +0900

    feat: 공개설정 페이지 백엔드 작업

--- Backend/src/main/java/com/runningcity/showroom/repository/UserTagRepository.java
+++ Backend/src/main/java/com/runningcity/showroom/repository/UserTagRepository.java
@@ -4,11 +4,16 @@ package com.runningcity.showroom.repository;
 import com.runningcity.showroom.entity.UserTag;
 import com.runningcity.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 
 public interface UserTagRepository extends JpaRepository<UserTag, Long> {
     List<UserTag> findAllByUserId(Long userId);
+
+    @Modifying
+    @Query(value = "DELETE FROM user_tags WHERE user_id = :userId", nativeQuery = true)
     int deleteAllByUserId(Long userId);
 }
